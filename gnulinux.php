2a483a13443eca6da21534da951e280e41d5d83c
Quentin Pradet <quentin.pradet@gmail.com>
2014-11-28 14:03:19 +0100

    Edit user-specific udev rules, not system-wide

--- gnulinux.php
+++ gnulinux.php
@@ -20,7 +20,7 @@
 <h2>3. Installer Firefox&nbsp;OS</h2>
 <ol>
     <li>Installez adb, un logiciel qui sert à envoyer des commandes à son téléphone. Tapez <code>sudo apt-get install android-tools-adb android-tools-fastboot</code> si vous êtes sous Debian, Ubuntu ou une autre dérivée de Debian. Sinon, utilisez votre gestionnaire de paquet habituel. Entrez votre mot de passe&nbsp;: apt-get va télécharger et installer adb.</li>
-    <li>Éditez les règles udev&nbsp;: toujours dans la console tapez <code>sudo nano /lib/udev/rules.d/91-permissions.rules</code>. Rendez-vous à la fin du fichier et ajoutez la ligne <code>SUBSYSTEM=="usb", MODE="0666", GROUP="plugdev"</code>. Sauvegardez en faisant CTRL+X, O, ENTRÉE.</li>
+    <li>Éditez les règles udev&nbsp;: toujours dans la console tapez <code>sudo nano /etc/udev/rules.d/91-permissions.rules</code>. Rendez-vous à la fin du fichier et ajoutez la ligne <code>SUBSYSTEM=="usb", MODE="0666", GROUP="plugdev"</code>. Sauvegardez en faisant Ctrl+X, O, Entrée.</li>
     <li>Redémarrez udev en tapant <code>sudo service udev restart</code> pour Ubuntu dans la console. Pour les autres systèmes d'exploitation, relancez le service udev de la manière habituelle ou redémarrez.</li>
     <li>Tapez <code>adb reboot recovery</code> dans la console, appuyez sur Entrée, le téléphone va redémarrer dans un mode spécial&nbsp;: le mode <em>recovery</em>.</li>
     <li>Sélectionnez <code>apply update from adb</code> en utilisant le bouton haut et bas du volume et appuyez sur le bouton de démarrage pour valider.</li>
